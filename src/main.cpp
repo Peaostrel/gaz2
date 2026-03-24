b2730b5c79bf0e093aba79fe6f6b14949ea22e73
Максим Акиншин <peaostrel@bk.ru>
2026-03-24 09:17:31 +0400

    Обновлен main.cpp: добавлено тестирование структуры и проверка исключений

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include "ArchiveManager.h"
+#include "FileSystemException.h"
 
 int main() {
-    std::cout << "Система виртуального архива gaz2 инициализирована." << std::endl;
+    // Поддержка кириллицы в консоли
+    setlocale(LC_ALL, "Russian");
+
+    try {
+        ArchiveManager archive;
+
+        std::cout << "--- Наполнение архива ---\n";
+        archive.addDirectory("Documents", AccessLevel::USER);
+        archive.addDirectory("SystemTools", AccessLevel::ADMIN);
+        
+        archive.addFile("readme", "txt", 1024);
+        archive.addFile("config", "cfg", 2048);
+        
+        archive.printTree();
+        archive.globalAudit();
+
+        std::cout << "--- Проверка системы валидации (ожидается перехват ошибки) ---\n";
+        // Пытаемся создать файл с запрещенным символом '*'
+        archive.addFile("virus*file", "exe", 9999); 
+
+    } catch (const FileSystemException& e) {
+        std::cerr << "[ОШИБКА ФС]: " << e.what() << "\n";
+    } catch (const std::exception& e) {
+        std::cerr << "[КРИТИЧЕСКАЯ ОШИБКА]: " << e.what() << "\n";
+    }
+
     return 0;
 }
